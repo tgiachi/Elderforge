dc242f14e23b772a6d49530108e62653a89fa3a9
Tom <squid@stormwind.it>
2024-11-19 15:31:32 +0100

    feat(BlockType.cs): add new block types (Dirt, Water, Sand, Gravel, Wood, Leaves, Glass, Brick, Snow, Ice) for game functionality and variety

--- src/Elderforge.Shared/Types/BlockType.cs
+++ src/Elderforge.Shared/Types/BlockType.cs
@@ -4,6 +4,16 @@ public enum BlockType : byte
 {
     Air,
     Grass,
+    Dirt,
     Stone,
     Rock,
+    Water,
+    Sand,
+    Gravel,
+    Wood,
+    Leaves,
+    Glass,
+    Brick,
+    Snow,
+    Ice
 }
